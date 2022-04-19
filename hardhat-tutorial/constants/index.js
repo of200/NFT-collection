c04a578ebb170bb083aea995c9f5788653c08757
olan griffin <olan.griffin.cbc1@gmail.com>
2022-04-19 18:19:42 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x02C8e36C156A568Fb77A76D659ba542Ef94401B6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
